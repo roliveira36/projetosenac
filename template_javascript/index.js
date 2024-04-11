7635437bd894633da20889108e3feb59805e9f73
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-11 11:37:26 -0300

    atividade template

---
+++ template_javascript/index.js
@@ -0,0 +1,23 @@
+const primeiroNome ="Rodrigo"
+const segundoNome ="Oliveira"
+const idadeNumero ="36"
+const serEstudante ="Sim"
+console.log("O seu nome e ",primeiroNome," o seu sobrenome e ",segundoNome, " idade ",idadeNumero," é estudante?" , serEstudante)
+
+/*let a = 10
+let b = 10
+console.log(b)
+b = 5
+console.log(a,b)
+
+let nome = prompt("Qual é seu nome?")
+alert(`Seu nome e ${nome}`)*/
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+console.log(a, b, c)
+
+
