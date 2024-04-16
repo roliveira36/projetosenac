7b5e66833495b5acbb5dd98ce35dd793027d4401
roliveira36 <rodrigoferreiraoliveira27@gmail.com>
2024-04-16 11:58:22 -0300

    Exercicios aula 16/04

---
+++ aula dia 15/index.js
@@ -0,0 +1,101 @@
+
+
+/*let resultado
+
+resultado = 3 + 4
+alert(resultado)
+
+resultado = (3+5)/2
+alert(resultado)
+
+resultado = 3+5/2
+alert(resultado)
+
+resultado =(4-5)*-1
+alert(resultado)
+
+resultado =234 % 5
+alert(resultado)
+
+//Leitura de dados informados pelo usuário
+primeiroNumero = prompt("Digite o primeiro numero:")
+segundoNumero = prompt("Digite o segundo numero:")
+
+//Testes de comparação
+resultado = primeiroNumero === segundoNumero
+
+//Mostra resultado na tela
+alert(resultado)
+
+//Leitura de dados informados pelo usuário
+primeiroNumero = prompt("Digite o primeiro numero:")
+segundoNumero = prompt("Digite o segundo numero:")
+
+//Teste de comparação
+resultado = primeiroNumero !== segundoNumero
+
+//Leitura de dados informados pelo usuário
+primeiroNumero = prompt("Digite o primeiro numero:")
+segundoNumero = prompt("Digite o segundo numero:")
+
+//Teste de comparação
+resultado = primeiro > segundoNumero
+
+//Declaração de variáveis
+let a
+let b
+let c
+let resultado
+
+//Leitura de dados informados pelo usuário
+a = true
+b = false
+c = true
+
+//1. Realize a operação: a && b
+resultado = a && b
+alert(resultado)
+//2. Realize a operação: b && c
+resultado = b && c
+alert(resultado)
+//3. Realize a operação: a && c
+resultado = a && c
+alert(resultado)
+//4. Realize a operação: a && b && c
+resultado = a && b && c
+alert(resultado)
+//1. Realize a operação: a || b
+resultado = a || b
+alert(resultado)
+//2. Realize a operação: b || c
+resultado = b || c
+alert(resultado)
+//3. Realize a operação: a || c
+resultado = a || c
+alert(resultado)
+//4. Realize a operação: a || b || c
+resultado = a || b || c
+alert(resultado)*/
+
+//Declaração de variavéis
+let nome
+let AnoNascimento 
+let AnoAtual 
+let idade 
+let MaiorIdade 
+let Idade2050
+//Calcula variavéis
+
+nome = prompt("Digite seu nome")
+AnoNascimento = prompt("AnoNascimento")
+AnoAtual = prompt("AnoAtual")
+
+//
+idade = AnoAtual - AnoNascimento
+MaiorIdade = idade >= 18 
+Idade2050 = 2050 - AnoNascimento
+//
+alert (nome)
+alert (idade)
+alert (MaiorIdade)
+alert (Idade2050)
