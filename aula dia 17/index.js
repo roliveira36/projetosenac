e80716ca953ee32bd1c065ad1a33bf5ccbfb7cfb
roliveira36 <rodrigoferreiraoliveira27@gmail.com>
2024-04-17 11:55:29 -0300

    atividade 17

---
+++ aula dia 17/index.js
@@ -0,0 +1,33 @@
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)
+
+//Variáveis
+
+let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+
+soma = primeiroNumero + segundoNumero
+alert(soma)*/
+
+//Escrita de código
+
+let idade = prompt("Digite sua idade")
+let idadeamigo = prompt("Digite idade do melhor amigo")
+soma = idade >=idadeamigo
+alert(idade)
+alert(idadeamigo)
+alert(`Sua idade é maior do que seu melhor amigo? ${soma}`)
+diferença = idade - idadeamigo
+alert(`diferença de idade ${diferença}`)
