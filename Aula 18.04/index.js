3ce416b678aa890b37b21807dc2c5e808a663e5e
roliveira36 <rodrigoferreiraoliveira27@gmail.com>
2024-04-18 11:51:27 -0300

    Atividade dia 18.04

---
+++ Aula 18.04/index.js
@@ -0,0 +1,21 @@
+/*let nome = prompt("Qual o seu nome?")
+let cor = prompt("Qual sua cor favorita?")
+alert(` Seu nome é ${nome} e sua cor favorita é ${cor}`)
+
+let nome = prompt("Qual o seu nome?")
+let idade = prompt("Qual sua idade?")
+alert("Seu nome é "+ nome + " e tem " + idade + "anos de idade ")
+
+//Exercício 2
+
+let frase = prompt("Digite uma frase:")
+console.log(frase.toUpperCase())
+console.log(frase.replaceAll("o" , "i"))
+console.log(frase.length)
+//console.log(frase.replaceAll("o" , "i"))*/
+
+//Exercício 3
+
+let racasdecachorro =["Vira lata", "Pastor alemão", "Golden", "Poodle", "Doberman", "Labrador "]
+let optionRaca = prompt("Digite um número de 0 a 4")
+alert(racasdecachorro[optionRaca])
