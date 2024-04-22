ea0e3bac39853c5d91f5967ef466f578ac96ccb7
roliveira36 <rodrigoferreiraoliveira27@gmail.com>
2024-04-22 11:57:58 -0300

    aula 22_04

--- Aula 18.04/index.js
+++ Aula 18.04/index.js
@@ -18,4 +18,49 @@ console.log(frase.length)
 
 let racasdecachorro =["Vira lata", "Pastor alemão", "Golden", "Poodle", "Doberman", "Labrador "]
 let optionRaca = prompt("Digite um número de 0 a 4")
-alert(racasdecachorro[optionRaca])*/
+alert(racasdecachorro[optionRaca])
+
+//Exercício 4
+
+let numeros = ["0", "1", "2", "3", "4", "5","6","7"]
+numeros.push(7)
+console.log(numeros)
+numeros.splice(3,2)
+console.log(numeros.length)
+console.log(numeros)
+
+//
+let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
+
+//
+
+const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+//Exercícios escrita de código
+
+let nome = prompt("Digite seu nome")
+let email =prompt("Digite seu email")
+alert (`O email ${email} foi cadastrado com sucesso. Seja bem-vindo(a), ${nome}`)
+
+//
+
+let comidasPreferidas = ["Pizza", "Xis", "Massa", "Lazanha", "Batata Frita"]
+console.log(comidasPreferidas[0] + "\n" + comidasPreferidas[1] + "\n" +comidasPreferidas[2] + "\n" +comidasPreferidas[3] + "\n" +comidasPreferidas[4])*/
