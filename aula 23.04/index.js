004c6caa74fa532a655498bfe2386f108c67cd8c
roliveira36 <rodrigoferreiraoliveira27@gmail.com>
2024-04-23 11:34:47 -0300

    atividade 23.04

---
+++ aula 23.04/index.js
@@ -0,0 +1,73 @@
+//Exercício 1
+/*--------------------------------------------------------
+let numUm = prompt("Digite um número")
+let numDois = prompt("Digite um número")
+
+if (numUm == numDois){
+    alert(`Sucesso!!!`)
+} else {
+    alert(`Não teve sucesso!!!`)
+
+
+//Exercício 2
+----------------------------------------------------------------------
+
+let numUm = prompt("Digite um número")
+let numDois = prompt("Digite um número")
+
+if (numUm == numDois){
+    alert(`Numeros iguais!!!`)
+} else {
+    alert(`Numeros diferentes!!!`)
+}
+
+//Exercício 3
+//----------------------------------------------------------------------
+
+let numUm = prompt("Digite um número")
+let numDois = prompt("Digite um número")
+
+if (numUm > numDois){
+    alert(`Numeros maior!!!`)
+} else if (numUm < numDois) {
+
+    alert(`Numeros menor!!!`)
+} else if (numUm == numDois){
+    alert(`Numeros iguais`) 
+} 
+
+//Exercício 4
+//----------------------------------------------------------------------------------
+
+pokemon = prompt("Digite seu primeiro pokemon: ")
+switch (pokemon){
+    case`Bulbassauro`:
+    alert(`Planta e Veneno!!!`)
+    break
+    case `Charmander`:
+        alert(`Fogo!!!`)
+        break
+        case `Squirtle`:
+            alert(`Água!!!`)
+            break
+            default:
+                alert(`Não é um pokemon inicial!!`)
+                break
+}
+------------------------------------------------------------------------------------------------
+//Exercício 5
+
+let cond1 = prompt ("Você concluiu o ensino médio?")
+let cond2 = prompt ("Você tem mais de 18 anos?")
+let cond3 = prompt ("Você está cursando outra faculdade?")
+//Condicional para faculdade
+if ((cond1 == "sim") && (cond2 == "sim") && (cond3 == "não")){
+    alert(`Você pode cursar a faculdade!!!`)
+} else {
+    alert(`Você NÃO pode cursar a faculdade!!!`)
+}*/
+
+
+
+
+
