40e6db0c0c113d3177a618d0e7c52401b85396f4
roliveira36 <rodrigoferreiraoliveira27@gmail.com>
2024-04-25 11:58:35 -0300

    Dia 25

---
+++ Aula dia 24.04 (25.04)/index.js
@@ -0,0 +1,41 @@
+// Exercício while
+
+/*let numero = 0
+let soma = 0
+numero = Number(prompt("Digite um número para ser somado: (para sair digite 0!!!"))
+
+while (numero != 0){
+    soma = soma + numero
+    numero = Number(prompt("Digite um número para ser somado: (Para sair digite 0!!!"))
+}
+alert(soma)*/
+
+//Exercícios Laços For
+
+/*let array = [11, 15, 18, 14, 12, 13]
+let maiorValor = 0
+
+for (let i = 0; i < 6; i++){
+    if (array[i] > maiorValor){
+        maiorValor = array[i]
+    }
+}
+alert(maiorValor)
+
+//Exercício 3 For...Of
+
+const palavras  = ["Oi" , "sumido(a)" , "tudo" , "bem?" , "Saudades"]
+let frase = ""
+
+for (let indices of palavras){
+    frase = frase + indices + " "
+}
+alert(frase)*/
+
+//Exercício
+
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
