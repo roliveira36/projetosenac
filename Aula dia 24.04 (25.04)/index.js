4eea7a93938b8076201c51af3be669348cb9e77b
roliveira36 <rodrigoferreiraoliveira27@gmail.com>
2024-04-30 11:53:32 -0300

    Aula 30_04

--- Aula dia 24.04 (25.04)/index.js
+++ Aula dia 24.04 (25.04)/index.js
@@ -30,7 +30,7 @@ let frase = ""
 for (let indices of palavras){
     frase = frase + indices + " "
 }
-alert(frase)*/
+alert(frase)
 
 //Exercício
 
@@ -39,3 +39,51 @@ for(let i = 0; i < 5; i++) {
   valor += i
 }
 console.log(valor)
+
+function imprimirOlaMundo(){
+    alert("Olá Mundo!")
+}
+
+imprimirOlaMundo()
+imprimirOlaMundo()
+imprimirOlaMundo()
+imprimirOlaMundo()
+imprimirOlaMundo()
+
+function imprimeNome(Nome){
+    alert(`Olá ${Nome}`)
+}
+
+let NomeUser=""
+NomeUser = prompt("Digite seu nome:")
+imprimeNome(NomeUser)
+
+//Exercício 3
+
+ let numero1 = 3
+ let numero2 = 12
+ let resultado = 0
+ resultado = somaNum(numero1,numero2)
+ alert(resultado)
+ 
+
+//Exercicio 4
+function trataArray(input){
+    //Inicializa variáveis
+    let array = []
+    //Armazena primeiro valor dividido por 2
+    array.push((input[0]/2))
+    array.push((input[(input.length-1)]/2))
+    //Retorna array nova
+    return array
+}
+
+//Declara varíaveis 
+let array = [2,3,4,5,6,7,8]
+let novaArray = []
+//Chama função
+novaArray = trataArray(array)
+//Escreve com alert
+alert(novaArray)*/
+
+
---
+++ Aula dia 30.04/index.js
@@ -0,0 +1,48 @@
+//Exercícios Interpretação de Código
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))
+
+
+function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+minhaFuncao(2)
+minhaFuncao(10)
+
+//Exercícios 2
+
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+function trocaParametros(){
+	let mensagem = "Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."
+	alert(mensagem)
+	alert(mensagem.replace("Caio", "Glauber").replace("23", "30").replace("São Paulo","São Leopoldo").replace("estudante", "Professor"))
+}
+trocaParametros()
+
+function parametros(nome,idade,cidade,profissão){
+
+
+	
+	console.log (`Sou ${nome} tenho ${idade} anos, moro em ${cidade} e sou ${profissão} `)
+}
+let nome = prompt("Digite o seu nome")
+	let idade = prompt("Digite a sua idade")
+	let cidade = prompt("Digite a sua cidade")
+	let profissão =prompt("Digite a sua profissão")
+
+parametros(nome,idade,cidade,profissão)*/
+
